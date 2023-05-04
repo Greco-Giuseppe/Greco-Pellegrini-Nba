7fbb2458430d2a9e512f5b48ca8c699693a4fb6d
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2023-05-04 10:42:14 +0000

    prima di inviare i dati

---
+++ templates_flask/app.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from flask import Flask
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="NBA2K20"
+)
+mycursor = mydb.cursor()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def unitList():
+    mycursor.execute("SELECT * FROM Clash_Unit")
+    myresult = mycursor.fetchall()
+    return render_template('clash_units.html', units=myresult)
